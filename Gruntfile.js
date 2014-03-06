6388472a1c20806373eaa4a83255e8aa2aff63e7
David Choi <kchoi79@hotmail.com>
2014-03-06 18:48:01 -0500

    built

--- Gruntfile.js
+++ Gruntfile.js
@@ -306,7 +306,8 @@ module.exports = function (grunt) {
                         'images/{,*/}*.webp',
                         '{,*/}*.html',
                         'styles/fonts/{,*/}*.*',
-                        'bower_components/' + (this.includeCompass ? 'sass-' : '') + 'bootstrap/' + (this.includeCompass ? 'fonts/' : 'dist/fonts/') +'*.*'
+                        'bower_components/' + (this.includeCompass ? 'sass-' : '') + 'bootstrap/' + (this.includeCompass ? 'fonts/' : 'dist/fonts/') +'*.*',
+                        'bower_components/font-awesome/{,*/}*.*'
                     ]
                 }]
             },
@@ -397,8 +398,7 @@ module.exports = function (grunt) {
         'copy:dist',
         'modernizr',
         'rev',
-        'usemin',
-        'htmlmin'
+        'usemin'
     ]);
 
     grunt.registerTask('default', [
